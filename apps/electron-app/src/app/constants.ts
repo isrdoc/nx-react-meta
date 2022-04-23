b7371e4bcd1e6eaef93f0f516327e0efcf7babe8
Igor Srdoč <igor@srdoc.si>
2022-04-24 00:26:41 +0200

    Add electron-app

---
+++ apps/electron-app/src/app/constants.ts
@@ -0,0 +1,4 @@
+export const rendererAppPort = 4200;
+export const rendererAppName = 'react-app';
+export const electronAppName = 'electron-app';
+export const updateServerUrl = 'https://deployment-server-url.com'; // TODO: insert your update server url here
--- libs/design-system/divider/src/lib/divider.tsx
+++ libs/design-system/divider/src/lib/divider.tsx
@@ -4,7 +4,7 @@ export interface DividerProps {}
 export function Divider(props: DividerProps) {
   return (
     <div>
-      <h1>Welcome to my Divider!</h1>
+      <h1>Welcome to the awesome Divider!</h1>
     </div>
   );
 }
