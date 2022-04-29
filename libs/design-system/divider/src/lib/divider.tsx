aefd4ebb8e50d115ab911ca04bd89517891450de
Igor Srdoč <igor@srdoc.si>
2022-04-29 05:52:04 +0200

    Should enable detox e2e testing for react-native-app

--- libs/design-system/divider/src/lib/divider.tsx
+++ libs/design-system/divider/src/lib/divider.tsx
@@ -4,7 +4,7 @@ export interface DividerProps {}
 export function Divider(props: DividerProps) {
   return (
     <div>
-      <h1 className="text-red-500">Welcome to the awesome Divider!</h1>
+      <h1 className="text-gray-500">Welcome to the awesome Divider!</h1>
     </div>
   );
 }
